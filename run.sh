fef87a608bafb9223721efa316aaea4f2f39b4ae
Martin Schijf <mschijf@gmail.com>
2025-02-08 06:36:36 +0100

    generalized run script with finding highest version, if version number is ommitted

--- run.sh
+++ run.sh
@@ -1,35 +1,41 @@
-if [ -z $1 ] || [ -z $2 ] || [ -z $3 ]
+if [ -z $1 ] || [ -z $2 ]
 then
-        echo "call is $0 {groupid} {artifact} {version}"
+        echo "call is $0 {groupid} {artifact} [{version}]"
         exit 0
 fi
 
 groupid=$1
 artifact=$2
-version=$3
 
+root_deployables=~/deployables
+deploy_path=$root_deployables/$groupid/$artifact
 
-base_path=~/$artifact
-log_path=$base_path/log
-program=$artifact-$version.jar
-port="8080"
-
-base_deployables=~/deployables
-deploy_path=$base_deployables/$groupid/$artifact/$version
-
-
-if [ ! -f $deploy_path/$program ]
+if [ -z $3 ]
 then
-	echo "$deploy_path/$program does not exist"
-	exit 0
+	version=`ls -dv $deploy_path/*/ | tail -1 | xargs -n 1 basename`
+else
+	version=$3
 fi
 
-mkdir -p $log_path
+
+deploy_version_path=$deploy_path/$version
+
+application_path=~/$artifact
+log_file=$application_path/log
+program=$deploy_version_path/$artifact-$version.jar
+port="8080"
+
+
+if [ ! -f $program ]
+then
+	echo "$program does not exist"
+	exit 0
+fi
 
 script_path=$(dirname $(realpath -s $0))
 bash $script_path/stop.sh $artifact
 
 
-java -Dspring.profiles.active=prod -Dserver.port=$port -jar $deploy_path/$program \
-     --spring.config.location=classpath:/application.yml,$base_path/application.yml \
-     >> $log_path/$artifact-log 2>> $log_path/$artifact-log &
+java -Dspring.profiles.active=prod -Dserver.port=$port -jar $program \
+     --spring.config.location=classpath:/application.yml,$application_path/application.yml \
+     >> $log_file 2>> $log_file &
